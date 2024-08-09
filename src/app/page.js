fb1a618114a10932326ea0d911ee056bd9f40705
Pranshu Patel <pranshu05patel@gmail.com>
2024-08-10 02:32:57 +0530

    Add basic UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
+import { Rammetto_One } from "next/font/google";
+
+const font = Rammetto_One({
+  weight: '400',
+  subsets: ['latin'],
+})
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello world!
-    </main>
+    <div className="w-full mx-auto">
+      <div className="h-dvh flex justify-center items-center mx-auto overflow-y-scroll bg-cover bg-fixed bg-center bg-no-repeat bgimg">
+        <h1 className={`text-7xl ${font.className}`}>FARM FUTURE ANALYTICS</h1>
+        
+      </div>
+      blah
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    </div>
   );
 }
