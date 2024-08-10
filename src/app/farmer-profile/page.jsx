60de18761a04f2936a2536f3f6856e13c3065d54
Abhishek Abbi <abhishekabbi.work@gmail.com>
2024-08-10 07:09:40 +0530

    feat(dashboard): added dashboard UI

---
+++ src/app/farmer-profile/page.jsx
@@ -0,0 +1,54 @@
+"use client"
+
+import Farmerhead from "@/components/farmer-dash/header";
+import Sidefarm from "@/components/farmer-dash/sidebar";
+import { useState } from "react";
+
+import Image from 'next/image';
+import DateDisplay from "@/components/farmer-dash/currdate";
+import ProfileCard from "@/components/farmer-dash/profile-card";
+import CropCard from "@/components/farmer-dash/cropcard";
+
+
+function Dash(){
+
+    const [welcome , Setwelcome] = useState("Good day, Kristin");
+
+    const [name , Setname] = useState("Kristin");
+
+    const [location , Setlocation] = useState("Gandhinagar, GJ");
+
+    return(
+
+        <body class="relative bg-[#f0f4d4] overflow-hidden max-h-screen">
+
+            {/* <Farmerhead section = "My Profile"/> */}
+
+            <Sidefarm/>
+
+  <main class="ml-60 pt-10 max-h-screen overflow-auto">
+    <div class="px-6 py-3">
+      <div class="max-w-4xl mx-auto">
+        <div class="bg-white rounded-3xl p-8 mb-5">          
+
+          <ProfileCard/>
+
+        
+        <h1 class="text-4xl font-bold mb-10 text-center text-black">My Crops</h1>
+        <div className="flex gap-2 items-start">
+
+        <CropCard cropname="Wheat"/>
+        <CropCard cropname="Potato"/>
+        </div>
+        
+        </div>
+      </div>
+    </div>
+  </main>
+
+
+</body>
+    )
+}
+
+export default Dash;
---
+++ src/components/farmer-dash/header.js
@@ -0,0 +1,22 @@
+
+
+export default function Farmerhead({section}){
+
+    return (
+
+        <header class="z-10 fixed right-0 top-0 left-60 bg-yellow-50 py-3 px-4 h-16">
+        <div class="max-w-4xl mx-auto">
+          <div class="flex items-center justify-between">
+            <div>
+              
+            </div>
+            <div class="text-3xl font-bold">{section}</div>
+            <div>
+             
+            </div>
+          </div>
+        </div>
+      </header>
+    
+    )
+}
