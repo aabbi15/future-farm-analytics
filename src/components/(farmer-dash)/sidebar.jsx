0ec0876bdc84343d3d357386d33b853fc3773eee
Abhishek Abbi <abhishekabbi.work@gmail.com>
2024-08-11 00:22:36 +0530

    updated sidebar

--- src/components/(farmer-dash)/sidebar.jsx
+++ src/components/(farmer-dash)/sidebar.jsx
@@ -41,9 +41,16 @@ export default function Sidefarm() {
                             </li>
                             <li>
                                 <a href="/crop-analysis" className={`flex bg-white    rounded-xl font-bold text-sm text-gray-900 py-3 px-4 ${isActive('/crop-analysis') ? 'bg-yellow-200' : 'hover:bg-yellow-50'}`}>
-                                    <Image src="/magnify.png" alt="" width={20} height={20} className="mr-4" />Personalized Analysis
+                                    <Image src="/magnify.png" alt="" width={20} height={20} className="mr-4" />My Price Analysis
                                 </a>
                             </li>
+
+                            <li>
+                                <a href="/soil-analysis" className={`flex items-center bg-white rounded-xl font-bold text-sm text-gray-900 py-3 px-4 ${isActive('/soil-analysis') ? 'bg-yellow-200' : 'hover:bg-yellow-50'}`}>
+                                    <Image src="/npk.png" alt="" width={20} height={20} className="mr-4" />N-P-K Soil Analysis
+                                </a>
+                            </li>
+
                             <li>
                                 <a href="/alerts" className={`flex bg-white    rounded-xl font-bold text-sm text-gray-900 py-3 px-4 ${isActive('/alerts') ? 'bg-yellow-200' : 'hover:bg-yellow-50'}`}>
                                     <Image src="/notify.png" alt="" width={20} height={20} className="mr-4" />All Alerts
@@ -55,20 +62,19 @@ export default function Sidefarm() {
                                 </a>
                             </li>
                             <li>
-                                <a href="/farmer-profile" className={`flex items-center bg-white rounded-xl font-bold text-sm text-gray-900 py-3 px-4 ${isActive('/profile') ? 'bg-yellow-200' : 'hover:bg-yellow-50'}`}>
+                                <a href="/farmer-profile" className={`flex items-center bg-white rounded-xl font-bold text-sm text-gray-900 py-3 px-4 ${isActive('/farmer-profile') ? 'bg-yellow-200' : 'hover:bg-yellow-50'}`}>
                                     <Image src="/farmer.png" alt="" width={20} height={20} className="mr-4" />My Profile
                                 </a>
                             </li>
                         </ul>
                     </div>
                 </div>
-                <div className="p-4 flex gap-2">
-                    <button type="button" className="rounded-md bg-[#124b3d] px-4 py-2 text-sm font-semibold text-white shadow-sm">
+                <div className="p-4">
+                    <button onClick={handleSignOut} type="button" className="flex gap-2 rounded-md bg-[#124b3d] px-4 py-2 text-sm font-semibold text-white shadow-sm">
                         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-sm" viewBox="0 0 16 16">
                             <path d="M12 1a1 1 0 0 1 1 1v13h1.5a .5 .5 0 0 1 0 1h-13a .5 .5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-                        </svg>
+                        </svg>Logout
                     </button>
-                    <button onClick={handleSignOut} className="rounded-md bg-[#124b3d] px-4 py-2 text-sm font-semibold text-white shadow-sm">Logout</button>
                 </div>
             </div>
         </aside>
